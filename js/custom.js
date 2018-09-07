8a02e9c95d22f8aa71214b8f50f9b231a1620d89
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-09-07 12:47:14 +0200

    Init basic slides

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+/**
+ * @param {HTMLElement} el
+ * @param {number} delay
+ */
+function shootingStar(el, delay) {
+  setTimeout(() => {
+    const topPos = Math.ceil(Math.random() * 90)
+    const leftPos = Math.ceil(Math.random() * 100)
+    const trans = Math.ceil(Math.random() * 300)
+    el.style.top = `${topPos}%`
+    el.style.left = `${leftPos}%`
+    el.style.transform = `rotate(${trans}deg)`
+    el.classList.add('shoot')
+  }, delay)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hallo Welt')
+
+  document.querySelectorAll('.shooting-star').forEach((element) => {
+    shootingStar(element, 1000 + Math.floor(Math.random() * 5000))
+  })
+})
